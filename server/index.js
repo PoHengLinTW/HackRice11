cb3d23631560d330fdcce442eb84de2583f82f9d
PoHengLinTW <ak47m245@gmail.com>
2021-09-18 21:38:18 +0000

    successfully set up video environment on html

--- server/index.js
+++ server/index.js
@@ -1,33 +1,73 @@
+require('dotenv').config()
 const express = require("express");
-
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
 const PORT = 8080;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+var twilioToken = ""
+client.tokens.create().then(token => {
+	twilioToken = token;
+	console.log(token)
+});
 
-const userList = [];
-const roomList = [];
+var userList = [];
 
 app.use(express.static('public'))
+
+app.get('/iceserver', (req, res) => {
+	return res.send(JSON.stringify(twilioToken.iceServers));
+})
+
+app.get('/user/:email', (req, res) => {
+	const user = userList.find(ele => ele.mail === req.params['email'])
+	if (user) res.send(user.sid);
+	else res.send("404");
+})
 
 io.on('connection', (socket) => {
 	console.log('a user connected', socket.id);
 	socket.on("get-gmail", (data) => {
-		console.log(data);
-		userList.add(data);
+		const newUser = {
+			mail: data.mail,
+			isInMeeting: false,
+			isOnline: true,
+			sid: socket.id,
+		}
+		userList.push(newUser);
+
 		socket.on("invite", (data) => {
-			socket.to(data.to).emit("get-invitation", data.from)
+			console.log("invite ", data);
+			socket.to(user.sid).emit("get-invitation-pending", {
+				from: data.from,
+				sid: socket.id
+			})
 		})
+
 		socket.on("candidate", (data) => {
 			socket.to(data.to).emit("candidate", data.candidate);
 		})
+
 		socket.on("send-offer", (data) => {
-			socket.to(data.to).emit("recv-offer", data.offer);
+			socket.to(data.to).emit("recv-offer", {
+				offer: data.offer,
+				from: socket.id
+			});
 		})
+
 		socket.on("send-answer", (data) => {
-			socket.to(data.to).emit("recv-answer", data.answer);
-		})
+			socket.to(data.to).emit("recv-answer", {
+				answer: data.answer,
+				from: socket.id
+			});
+		})	
+	})
+	socket.on("disconnect", () => {
+		console.log(socket.id + " leave ");
+		userList = userList.filter(ele => ele.sid !== socket.id);
+		socket.broadcast.emit("user-leave", socket.id);
 	})
 });
 
--- server/public/webRTC.js
+++ server/public/webRTC.js
@@ -1,14 +1,29 @@
 
-const {RTCPeerConnection, RTCIceCandidate} = window
 
 const myInput = document.getElementById('myId');
 const join = document.getElementById('join');
 const invitation = document.getElementById('otherId');
 const send = document.getElementById('send');
-var localStream;
+const videoDiv = document.getElementById('video-div');
+var localStream = new MediaStream();
+var videoEle;
+var opponentSid = "";
 
 /** ------------------- */
+const {RTCPeerConnection, RTCSessionDescription, RTCIceCandidate} = window
+var config = {
+    iceServers: [ { urls: ["stun:stun.l.google.com:19302"] } ] 
+};
+fetch('https://oneclickmeeting.tech/iceserver')
+.then(res => {return res.json()})
+.then( data => {
+    config.iceServers = data;
+    console.log(config)
+})
+var myPC = new RTCPeerConnection(config);
+
 const mediaStreamConstraints = {
+    audio: true,
     video: true,
 };
 
@@ -17,26 +32,92 @@ function handleLocalMediaStreamError(error) {
 }
   
 
-const socket = io("http://34.125.144.188/");
+const socket = io("https://oneclickmeeting.tech/");
 
 socket.on("connect", () => {
-  console.log(socket.id); 
-  socket.on("welcome", (data) => {console.log(data)});
+    myPC.onicecandidate = (evt) => {
+        console.log("candidate")
+        socket.emit("candidate", {
+            to: opponentSid,
+            candidate: evt.candidate
+        })
+    }
+
+    myPC.ontrack = event => {
+        if (!document.getElementById(event.streams[0].id)) {
+            videoEle = document.createElement('video');
+            videoEle.autoplay = true;
+            videoEle.playsInline = true;
+            videoEle.srcObject = event.streams[0];
+            videoEle.id = event.streams[0].id;
+            videoDiv.appendChild(videoEle);
+        }
+    };
+
+    console.log(socket.id); 
+
+    socket.on("recv-offer", (data) => {
+        console.log("recv-offer")
+
+        navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+        .then(gotLocalMediaStream).then(()=>{
+            console.log(localStream)
+            localStream.getTracks().forEach(track => myPC.addTrack(track, localStream));
+            myPC.setRemoteDescription(new RTCSessionDescription(data.offer));
+            myPC.createAnswer().then(answer => {
+                myPC.setLocalDescription(answer);
+                socket.emit("send-answer", {
+                    to: data.from,
+                    answer: answer
+                })
+            });
+        }).catch(handleLocalMediaStreamError);
+    })
+    socket.on("recv-answer", (data) => {
+        console.log("recv-answer")
+        myPC.setRemoteDescription(new RTCSessionDescription(data.answer));
+    })
+    socket.on("candidate", (candidate) => {
+        console.log("candidate")
+        if (candidate) myPC.addIceCandidate(candidate)
+        else console.log("candidate ends");
+    })
+    
+    socket.on("user-leave", (sid) => {
+        console.log("user-leave", sid, opponentSid)
+        if (sid === opponentSid) {
+            localStream.getTracks().forEach( track => track.stop);
+            videoDiv.innerHTML = "";
+        }
+    })
 });
 
 join.onclick = (evt) => {
     socket.emit("get-gmail", {
-        mail: myInput.innerHTML
+        mail: myInput.value
     })
 }
 
-send.onclick = (evt) => {
-    console.log("click")
-    navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
-    .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
-    // socket.emit("invite", {
-    //     mail: myInput.innerHTML
-    // })
+send.onclick = async (evt) => {
+    fetch("https://oneclickmeeting.tech/user/" + invitation.value)
+    .then(res => {return res.text()})
+    .then(data => {
+        console.log(data);
+        opponentSid = data;
+        navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+        .then(gotLocalMediaStream).then(()=>{
+            localStream.getTracks().forEach(track => myPC.addTrack(track, localStream));
+            myPC.createOffer().then(offer => {
+                myPC.setLocalDescription(offer);
+                socket.emit("send-offer", {
+                    to: opponentSid,
+                    offer: offer
+                })
+            });
+        }).catch(handleLocalMediaStreamError);
+    });
+    
+
 }
 
 function gotLocalMediaStream(mediaStream) {
