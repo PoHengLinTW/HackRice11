2d2b61c4bb3cee6fb38b51bd4fa244e1364c3824
Di Wu <wud2@union.edu>
2021-09-19 05:16:59 -0500

    final commit

--- chrome_extension/mainpopup.js
+++ chrome_extension/mainpopup.js
@@ -49,8 +49,6 @@ function callback(tabs) {
     console.log(currentTab);
     var tabId = currentTab['id'];
 
-    url = currentTab.url;
-    url = url.replace(/[\[\]&]+/g, '');
 
     console.log(tabId);
     chrome.scripting.executeScript({
@@ -72,16 +70,23 @@ function callback(tabs) {
         injectionResults
     );
 
-    console.log(url);
+
 }
 
 function loadStarredUsers() {
-    
+
 }
 
 function loadGroups() {
 
 }
+
+function getUrl(tabs) {
+    currentTab = tabs[0];
+    url = currentTab.url;
+    console.log(url);
+}
+
 
 function addStarredUser() {
     var addStarredButton = document.getElementsByClassName("starredButton");
@@ -107,12 +112,12 @@ function addStarredUser() {
                 dot.style.background = "gray";
             }
             var profile = document.createElement("img");
-            profile.src = "images/icon16.png";
+            profile.src = "images/user.png";
             var name = "Di Wu";
             var callButton = document.createElement("BUTTON");
             callButton.textContent = "Video Call";
             callButton.classList.add("callButton");
-            callButton.addEventListener("click",function(){
+            callButton.addEventListener("click", function () {
                 var myemail;
                 embedVideo();
             });
@@ -164,23 +169,27 @@ function embedVideo() {
 
 let email = getEmail();
 
-
+chrome.tabs.query(query, getUrl);
 $("#adding").click(function () {
     //var addGroupButton = document.getElementsByClassName("addGroup");
     var i;
 
     console.log("breakpoint 1");
-    
+    console.log(url);
+
     //for (i = 0; i < addGroupButton.length; i++) {
-        //addGroupButton.addEventListener("click", function () {
-       //     chrome.tabs.query(query, callback);
-            fetch('http://oneclickmeeting.tech:8081/getUser/email/' + email, {})
-            .then(res => res.json())
-            .then(res => {
-                console.log(res)
-                if(res === null) {
-                    var userName = prompt("Please enter your name: "); // ask for username
-                    fetch('http://oneclickmeeting.tech:8081/addUser', {
+    //addGroupButton.addEventListener("click", function () {
+
+    url = url.replace(/[^a-zA-Z0-9]/g, "");
+    console.log(url);
+    var groupName = "";
+    fetch('http://oneclickmeeting.tech:8081/getUser/email/' + email, {})
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            if (res === null) {
+                var userName = prompt("Please enter your name: "); // ask for username
+                fetch('http://oneclickmeeting.tech:8081/addUser', {
                         method: 'post',
                         headers: {
                             'Accept': 'application/json, text/plain, /',
@@ -192,60 +201,97 @@ $("#adding").click(function () {
                         })
                     }).then(res => res.json())
                     .then(res => console.log(res));
-                }
-                fetch('http://oneclickmeeting.tech:8081/getGroup/id/' + url, {})
+            }
+            fetch('http://oneclickmeeting.tech:8081/getGroup/id/' + url, {})
                 .then(res => res.json())
                 .then(res => {
                     console.log(res)
-                    if(res === null) {
-                        var groupName = prompt("Please enter your group name: "); // ask for group name
+                    if (res === null) {
+                        groupName = prompt("Please enter your group name: "); // ask for group name
+                        console.log(groupName);
+                        setGroupName(groupName);
                         fetch('http://oneclickmeeting.tech:8081/addGroup', {
-                            method: 'post',
-                            headers: {
-                                'Accept': 'application/json, text/plain, /',
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify({
-                                "name": groupName,
-                                "groupId": url
-                            })
-                        }).then(res => res.json())
-                        .then(res => console.log(res));
+                                method: 'post',
+                                headers: {
+                                    'Accept': 'application/json, text/plain, /',
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({
+                                    "name": groupName,
+                                    "groupId": url
+                                })
+                            }).then(res => res.json())
+                            .then(res => console.log(res));
                     }
-                    fetch('http://oneclickmeeting.tech:8081/addUserToGroup', {
-                        method: 'post',
-                        headers: {
-                            'Accept': 'application/json, text/plain, /',
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            "email": email,
-                            "groupId": url
-                        })
-                    }).then(res => res.json())
-                    .then(res => console.log(res));
                 });
-            });
-            
-            var groupList = document.getElementById("groupList");
-            var li = document.createElement("li");
-            var groupHeader = document.createElement("BUTTON");
-            groupHeader.textContent = "Fill the group number, APP!"; // need webpage information to get group number
-            groupHeader.setAttribute("class", "groupCollapsible");
+        });
 
-            // HERE: need to load the group members if the group already exists, otherwise the empty group
 
-            li.appendChild(groupHeader);
-            // li.setAttribute("id", groupList.length.toString());
-            groupList.appendChild(li);
-            //alert(li.id);
-        //})
+    // HERE: need to load the group members if the group already exists, otherwise the empty group
+
+
+    //})
     //};
 
-  //embedVideo();
+    //embedVideo();
     //updateStatue();
 });
 
+
+function setGroupName(groupName) {
+    console.log(groupName);
+    var groupList = document.getElementById("groupList");
+    var li = document.createElement("li");
+    var groupHeader = document.createElement("BUTTON");
+    groupHeader.textContent = groupName; // need webpage information to get group number
+    groupHeader.setAttribute("class", "groupCollapsible");
+    groupHeader.classList.add("collapsible");
+
+    // li.setAttribute("id", groupList.length.toString());
+    groupList.appendChild(li);
+    //alert(li.id);
+    var table = document.createElement("table");
+    var row = table.insertRow(0);
+
+    // 5 columns: online status, profile, name, video call button, starred button
+    var statusCell = row.insertCell(0);
+    var profileCell = row.insertCell(1);
+    var nameCell = row.insertCell(2);
+    var callButtonCell = row.insertCell(3);
+    var starredButtonCell = row.insertCell(4);
+
+    var onlineStatus = true; // need to fetch from WebRTC to know who is online
+    var dot = document.createElement("div");
+    dot.classList.add("dot");
+    if (onlineStatus) {
+        dot.style.backgroundColor = "green";
+    } else {
+        dot.style.background = "gray";
+    }
+    var profile = document.createElement("img");
+    profile.src = "images/user.png";
+    var name = "Di Wu";
+    var callButton = document.createElement("BUTTON");
+    callButton.textContent = "Video Call";
+    callButton.classList.add("callButton");
+    callButton.addEventListener("click", function () {
+        var myemail;
+        embedVideo();
+    });
+    var starredButton = document.createElement("BUTTON");
+    starredButton.textContent = "Start";
+    starredButton.classList.add("starredButton");
+
+    statusCell.appendChild(dot);
+    profileCell.appendChild(profile);
+    nameCell.textContent = name;
+    callButtonCell.appendChild(callButton);
+    starredButtonCell.appendChild(starredButton);
+
+    li.appendChild(groupHeader);
+    li.appendChild(table);
+    groupList.appendChild(li);
+}
 // function calls
 dropDown();
 loadStarredUsers();
